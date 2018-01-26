caa3ceb2b417789d6a4ecd58bc5e73b526a08daa
Dongsheng Han <dongsheng1han@gmail.com>
2018-01-26 00:35:32 -0800

    add a menu and the tower that has already been added can not be added another tower that would replace the old one anymore

--- go/main.js
+++ go/main.js
@@ -5,7 +5,7 @@ function GameBoard(game) {
     this.grid = false;
     this.player = 1;
     this.board = [];
-    for (var i = 0; i < 18; i++) {
+    for (var i = 0; i < 22; i++) {
         this.board.push([]);
         for (var j = 0; j < 18; j++) {
             this.board[i].push(0);
@@ -17,20 +17,31 @@ GameBoard.prototype = new Entity();
 GameBoard.prototype.constructor = GameBoard;
 
 GameBoard.prototype.update = function () {
-    if (this.game.click && this.game.click.x > -1 && this.game.click.x < 18 && this.game.click.y > -1 && this.game.click.y < 18 ) {
+    if (this.game.click 
+		&& this.game.click.x > -1 && this.game.click.x < 18 && this.game.click.y > -1 && this.game.click.y < 18 
+		&& this.board[this.game.click.x][this.game.click.y] === 0) {
         this.board[this.game.click.x][this.game.click.y] = this.player;
+		
         // this.player = this.player === 1 ? 2 : 1;
+    }
+	if (this.game.click && this.game.click.x === 21 && this.game.click.y === 1) {
+        this.player = 1;
+    }
+	if (this.game.click && this.game.click.x === 21 && this.game.click.y === 2) {
+        this.player = 2;
     }
     Entity.prototype.update.call(this);
 }
 
 GameBoard.prototype.draw = function (ctx) {
     ctx.drawImage(ASSET_MANAGER.getAsset("./img/960px-Blank_Go_board.png"), this.x, this.y, 800, 800);
-
+	
     var size = 41.67;
     var offset = 45;
+	ctx.drawImage(ASSET_MANAGER.getAsset("./img/black.png"), 21 * size + offset, size + offset, 40, 40);
+	ctx.drawImage(ASSET_MANAGER.getAsset("./img/white.png"), 21 * size + offset, 2 * size + offset, 40, 40);
 
-    for (var i = 0; i < 18; i++) {
+    for (var i = 0; i < 22; i++) {
         for (var j = 0; j < 18; j++) {
 			// shows the grid of each image placement
             ctx.strokeStyle = "Green";
@@ -39,9 +50,9 @@ GameBoard.prototype.draw = function (ctx) {
             if (this.board[i][j] === 1) {
 				ctx.drawImage(ASSET_MANAGER.getAsset("./img/black.png"), i * size + offset, j * size + offset, 40, 40);
             }
-            // if (this.board[i][j] === 2) {
-				// ctx.drawImage(ASSET_MANAGER.getAsset("./img/white.png"), i * size + offset, j * size + offset, 40, 40);
-            // }
+            if (this.board[i][j] === 2) {
+				ctx.drawImage(ASSET_MANAGER.getAsset("./img/white.png"), i * size + offset, j * size + offset, 40, 40);
+            }
         }
     }
 
@@ -50,7 +61,7 @@ GameBoard.prototype.draw = function (ctx) {
         ctx.save();
         ctx.globalAlpha = 0.5;
         if(this.player === 1) ctx.drawImage(ASSET_MANAGER.getAsset("./img/black.png"), this.game.mouse.x * size + offset, this.game.mouse.y * size + offset, 40, 40);
-        // else ctx.drawImage(ASSET_MANAGER.getAsset("./img/white.png"), this.game.mouse.x * size + offset, this.game.mouse.y * size + offset, 40, 40);
+        if(this.player === 2)  ctx.drawImage(ASSET_MANAGER.getAsset("./img/white.png"), this.game.mouse.x * size + offset, this.game.mouse.y * size + offset, 40, 40);
         ctx.restore();
     }
 }
