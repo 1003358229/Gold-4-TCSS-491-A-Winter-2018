ce7863ee9aa9e8a7ed57dd5aaeea56cbaef53d4d
Dongsheng Han <dongsheng1han@gmail.com>
2018-02-23 17:24:46 -0800

    keep attacking the same target when bullet already shooted but not yet hit

--- Gold four TD/main.js
+++ Gold four TD/main.js
@@ -1,23 +1,19 @@
-//Edited by 
+//TCSS491 2018
+
+//Edited by
 //Dongsheng Han
 //Karan Kurbur
 //Dirk Sexton
 
-//TCSS491 2018
 
 //Gameboard
-
-
-
 function GameBoard(game) {
-
     Entity.call(this, game, 20, 20);
-	this.radius_x = 0;
-	this.radius_y = 0;
-	this.display_radius = false;
+	this.range_x = 0;
+	this.range_y = 0;
+	this.display_range = false;
 	this.score = 0;
 	this.money = 1000;
-    //this.grid = false;
     this.canBuy = true;
 	this.purchaes_and_placed = false;
     this.player = 0;
@@ -47,7 +43,7 @@ GameBoard.prototype.update = function () {
 			purchaes_and_placed = false;
 
 			//Using this.canBuy to determine if to draw shadow and if you can place another tower
-			//TODO: modify this.canBuy outside of click loop to fix small bug where you
+			//DONE: modify this.canBuy outside of click loop to fix small bug where you
 			//have less than price of current tower but haven't clicked since then 
 			//Money subtraction
 			//put all four statement in to one
@@ -82,16 +78,16 @@ GameBoard.prototype.update = function () {
 				this.canBuy = false;
 			}
 			
-			//not draw radius for tower not already been placed
-			this.radius_x = this.game.click.x;
-			this.radius_y = this.game.click.y;
-			this.display_radius = false;
+			//not draw range for tower not already been placed
+			this.range_x = this.game.click.x;
+			this.range_y = this.game.click.y;
+			this.display_range = false;
 		}
-		//draw radius for tower already been placed
+		//draw range for tower already been placed
 		if(this.board[this.game.click.x][this.game.click.y] != 0){
-			this.radius_x = this.game.click.x;
-			this.radius_y = this.game.click.y;
-			this.display_radius = true;
+			this.range_x = this.game.click.x;
+			this.range_y = this.game.click.y;
+			this.display_range = true;
 		}
 
     }
@@ -141,21 +137,18 @@ GameBoard.prototype.draw = function (ctx) {
 	
 	//Score and money
 	ctx.font = "25px Arial";
-
 	ctx.fillStyle = 'white';
 	ctx.fillText("Score: " + this.score, 45, 40); 
 	ctx.fillText("Money: " + this.money, 420, 40); 
-	ctx.strokeStyle = "white";
-	
-	ctx.strokeText("Towers", 865, 40); 
-	ctx.strokeText("$50", 930, 120); 
-	ctx.strokeText("$100", 930, 200); 
-	ctx.strokeText("$75", 930, 280); 
-	ctx.strokeText("$125", 930, 360); 
-	ctx.strokeText("$150", 930, 440); 
-	ctx.strokeText("$275", 930, 520); 
-	ctx.strokeText("$150", 930, 600); 
-	ctx.strokeText("$350", 930, 680); 
+    ctx.fillText("Towers", 865, 40); 
+    ctx.fillText("$50", 930, 120); 
+    ctx.fillText("$100", 930, 200); 
+    ctx.fillText("$75", 930, 280); 
+    ctx.fillText("$125", 930, 360); 
+    ctx.fillText("$150", 930, 440); 
+    ctx.fillText("$275", 930, 520); 
+    ctx.fillText("$150", 930, 600); 
+    ctx.fillText("$350", 930, 680); 
  
 	//Draw tower menu
 	ctx.drawImage(ASSET_MANAGER.getAsset("./img/tower1.png"), 20 * size + offset, size + offset - 20, 40, 60);
@@ -167,7 +160,7 @@ GameBoard.prototype.draw = function (ctx) {
 	ctx.drawImage(ASSET_MANAGER.getAsset("./img/tower7.png"), 20 * size + offset, 13 * size + offset - 20, 40, 60);
 	ctx.drawImage(ASSET_MANAGER.getAsset("./img/tower8.png"), 20 * size + offset, 15 * size + offset - 20, 40, 60);
 	
-    // draw mouse shadow with tower and its radius
+    // draw mouse shadow with tower and its range
     if (this.game.mouse && this.canBuy && !this.purchaes_and_placed) {
 		ctx.save();
         ctx.globalAlpha = 0.25;
@@ -180,7 +173,7 @@ GameBoard.prototype.draw = function (ctx) {
 		if(this.player === 7) ctx.drawImage(ASSET_MANAGER.getAsset("./img/tower7.png"), this.game.mouse.x * size + offset, this.game.mouse.y * size + offset - 20, 40, 60);
         if(this.player === 8) ctx.drawImage(ASSET_MANAGER.getAsset("./img/tower8.png"), this.game.mouse.x * size + offset, this.game.mouse.y * size + offset - 20, 40, 60);
 
-        //draw radius for tower
+        //draw range for tower
         if (this.player != 0) {
             ctx.beginPath();
             ctx.rect(offset + (this.game.mouse.x - 2) * size, offset + (this.game.mouse.y - 2) * size, size * 5, size * 5);
@@ -191,10 +184,10 @@ GameBoard.prototype.draw = function (ctx) {
         ctx.restore();
 	}
 
-	//draw radius for tower when clicked
-	if(this.board[this.radius_x][this.radius_y] != 0){
+	//draw range for tower when clicked
+	if(this.board[this.range_x][this.range_y] != 0){
 		ctx.beginPath();
-		ctx.rect(offset + (this.radius_x-2)*size, offset + (this.radius_y-2)*size, size * 5, size * 5);
+		ctx.rect(offset + (this.range_x-2)*size, offset + (this.range_y-2)*size, size * 5, size * 5);
 		ctx.lineWidth="5";
         ctx.strokeStyle ="blue";
 		ctx.stroke();
@@ -217,7 +210,6 @@ function Animation(spriteSheet, startX, startY, frameWidth, frameHeight, frameDu
     this.frames = frames;
     this.loop = loop;
     this.scale = scale;
-	
 	
     this.totalTime = frameDuration * frames;
     this.elapsedTime = 0;
@@ -260,27 +252,61 @@ function tower1(game,x,y) {
     this.boardY = y;
     this.game = game;
     this.ctx = game.ctx;
-    this.towerRange = 2; //Can attack 5x5 square with tower in center.
     this.animation = new Animation(ASSET_MANAGER.getAsset("./img/cannonball.png"), 0, 0, 420, 420, 0.2, 1, true, 0.1);
     this.x;
     this.y;
     this.size = 41.67;
     this.offset = 45;
-    //this.attacker = 0;
-    this.fire = false;
-    this.fire_distance = 0;
-    this.fireRate = 25;
+    this.tower;//a variable to hold tower type
+    this.fire = false;//boolean use to decide weatrher the enemy is in range
+    this.fire_distance = 0;//this is use to keep track of the bullet position
+    this.towerRange; //EX = 2; Can attack 5x5 square with tower in center.
+    this.fireRate; //this is the bullet speed and fire rate of the tower
+    this.attackingEnt = null;//keep the bullet attacking same enemy when walking out of range
+    this.inRangeEnt;//keep the bullet attacking same enemy when walking out of range
 }
-
 
 tower1.prototype = new Entity();
 tower1.prototype.constructor = tower1;
 
 tower1.prototype.draw = function (ctx) {
-    var player = this.game.entities[0].board[this.boardX][this.boardY];
-    var joined = ['./img/tower', player, '.png'].join('');
-	
+    //draw tower base on the infor stored in the game board
+    this.tower = this.game.entities[0].board[this.boardX][this.boardY];
+    var joined = ['./img/tower', this.tower, '.png'].join('');
     ctx.drawImage(ASSET_MANAGER.getAsset(joined), this.boardX * this.size + this.offset, this.boardY * this.size + this.offset - 20, 40, 60);
+    if (this.tower == 1){
+        this.towerRange = 2; //Can attack 5x5 square with tower in center.
+        this.fireRate = 50; //this is the bullet speed and fire rate of the tower
+    }
+    if (this.tower == 2) {
+        this.towerRange = 2; //Can attack 5x5 square with tower in center.
+        this.fireRate = 25; //this is the bullet speed and fire rate of the tower
+    }
+    if (this.tower == 3) {
+        this.towerRange = 2; //Can attack 5x5 square with tower in center.
+        this.fireRate = 25; //this is the bullet speed and fire rate of the tower
+    }
+    if (this.tower == 4) {
+        this.towerRange = 2; //Can attack 5x5 square with tower in center.
+        this.fireRate = 25; //this is the bullet speed and fire rate of the tower
+    }
+    if (this.tower == 5) {
+        this.towerRange = 2; //Can attack 5x5 square with tower in center.
+        this.fireRate = 25; //this is the bullet speed and fire rate of the tower
+    }
+    if (this.tower == 6) {
+        this.towerRange = 2; //Can attack 5x5 square with tower in center.
+        this.fireRate = 25; //this is the bullet speed and fire rate of the tower
+    }
+    if (this.tower == 7) {
+        this.towerRange = 2; //Can attack 5x5 square with tower in center.
+        this.fireRate = 25; //this is the bullet speed and fire rate of the tower
+    }
+    if (this.tower == 8) {
+        this.towerRange = 2; //Can attack 5x5 square with tower in center.
+        this.fireRate = 25; //this is the bullet speed and fire rate of the tower
+    }
+    //draw bullet
     if (this.fire){
         this.animation.drawFrame(this.game.clockTick, ctx, this.x, this.y);
     }
@@ -288,14 +314,19 @@ tower1.prototype.draw = function (ctx) {
 };
 
 tower1.prototype.update = function (ctx) {
+    //loop through all entities in game engine
     var isInrange;
     this.fire = false;
     for (var i = 0; i < this.game.entities.length; i++) {
         var ent = this.game.entities[i];
-        if (this != ent && ent.attacker == 1) { //Entity is an enemy, should check to see if it is in range.
+        if (this != ent && ent.attacker == 1) { //if Entity is an enemy, should check to see if it is in range.
             isInrange = this.inRange(ent.boardX, ent.boardY);
             if (isInrange) {
                 this.fire = true;
+                this.inRangeEnt = ent;
+                if (this.attackingEnt == null) {
+                    this.attackingEnt = this.inRangeEnt;
+                }
                 break;
             } else {
                 this.fire = false;
@@ -303,18 +334,28 @@ tower1.prototype.update = function (ctx) {
         };
     };
 
+    //keep attacking the same target when bullet already shooted but not yet hit
+    if (this.attackingEnt != this.inRangeEnt
+        && !this.inRange(this.attackingEnt.boardX, this.attackingEnt.boardY)
+        && this.fire_distance != 0) {
+        this.fire = true;
+    } else {
+        this.attackingEnt = this.inRangeEnt;
+    }
+
+    //if the enemy is in range, shoot and change health and score
     if (this.fire) {
-        (ent.x + 6) < (this.boardX * this.size + this.offset)
+        (this.attackingEnt.x + 6) < (this.boardX * this.size + this.offset)
             ? this.x = (this.boardX * this.size + this.offset)
-            - this.fire_distance * Math.abs((ent.x + 6) - (this.boardX * this.size + this.offset)) / this.fireRate
+            - this.fire_distance * Math.abs((this.attackingEnt.x + 6) - (this.boardX * this.size + this.offset)) / this.fireRate
             : this.x = (this.boardX * this.size + this.offset)
-            + this.fire_distance * Math.abs((ent.x + 6) - (this.boardX * this.size + this.offset)) / this.fireRate;
+            + this.fire_distance * Math.abs((this.attackingEnt.x + 6) - (this.boardX * this.size + this.offset)) / this.fireRate;
         
-        (ent.y + 10) < (this.boardY * this.size + this.offset)
+        (this.attackingEnt.y + 10) < (this.boardY * this.size + this.offset)
             ? this.y = (this.boardY * this.size + this.offset)
-            - this.fire_distance * Math.abs((ent.y + 10) - (this.boardY * this.size + this.offset)) / this.fireRate
+            - this.fire_distance * Math.abs((this.attackingEnt.y + 10) - (this.boardY * this.size + this.offset)) / this.fireRate
             : this.y = (this.boardY * this.size + this.offset)
-            + this.fire_distance * Math.abs((ent.y + 10) - (this.boardY * this.size + this.offset)) / this.fireRate;
+            + this.fire_distance * Math.abs((this.attackingEnt.y + 10) - (this.boardY * this.size + this.offset)) / this.fireRate;
 
         if (this.fire_distance < this.fireRate) {
             this.fire_distance++;
@@ -328,6 +369,7 @@ tower1.prototype.update = function (ctx) {
     Entity.prototype.update.call(this);
 };
 
+//use to test if the enemy is in range, currently using a square
 tower1.prototype.inRange = function (x, y) {
     return x <= this.boardX + this.towerRange
         && x >= this.boardX - this.towerRange
@@ -353,7 +395,7 @@ function attackDude(game) {
     this.y_offset = 35;
     this.x = this.x_offset;
     this.y = Math.ceil(this.y_offset + this.size * 5);
-    this.attacker = 1;
+    this.attacker = 1;//use to indicate that this is a enemy(tower1.prototype.update)
     this.game = game;
     this.ctx = game.ctx;
     this.boardX = 0;
@@ -365,7 +407,8 @@ attackDude.prototype = new Entity();
 attackDude.prototype.constructor = attackDude;
 
 attackDude.prototype.draw = function (ctx) {
-	ctx.save();
+    //draw health
+    ctx.save();
 	ctx.strokeStyle ="red";
 	if(this.direction === 1){
 		if(this.health > 0){
@@ -391,6 +434,9 @@ attackDude.prototype.draw = function (ctx) {
 			this.animationD.drawFrame(this.game.clockTick, ctx, this.x, this.y);
 		}
     }
+
+    //delete entities when health = 0
+    //TODO: draw death animation
     if (this.health <= 0) {
 		//this.death.drawFrame(this.game.clockTick, ctx, this.x, this.y);
 		//var snd = new Audio("explosion.mp3"); // buffers automatically when created
@@ -420,19 +466,18 @@ attackDude.prototype.draw = function (ctx) {
 
 
 	ctx.restore();
-	/*
-    //red circle around dog
-    ctx.beginPath();
-    ctx.lineWidth="6";
-    ctx.strokeStyle="red";
-    ctx.rect(this.x + 6, this.y + 10, 1, 1);
-    ctx.stroke();
-    ctx.lineWidth="1";
-    ctx.strokeStyle="red";
-    ctx.rect(this.x + 6, this.y + 10, this.size, this.size);
-    ctx.stroke();
-	
-*/
+
+    ////red circle around dog
+    //ctx.beginPath();
+    //ctx.lineWidth="6";
+    //ctx.strokeStyle="red";
+    //ctx.rect(this.x + 6, this.y + 10, 1, 1);
+    //ctx.stroke();
+    //ctx.lineWidth="1";
+    //ctx.strokeStyle="red";
+    //ctx.rect(this.x + 6, this.y + 10, this.size, this.size);
+    //ctx.stroke();
+
 	Entity.prototype.draw.call(this);
 }
 
@@ -440,51 +485,24 @@ attackDude.prototype.draw = function (ctx) {
 
 attackDude.prototype.update = function () {
 
-    // walk in the square for now
     // walk to 3,5
     if (this.x < Math.ceil(this.x_offset + this.size * 3)
         && this.y === Math.ceil(this.y_offset + this.size * 5)) {
         this.direction = 1;
         this.x += 0.25 * 4;
-        //walk to 3,7
+    //walk to 3,7
     } else if (this.x === Math.ceil(this.x_offset + this.size * 3)
         && this.y < Math.ceil(this.y_offset + this.size * 7)) {
         this.direction = 4;
         this.y += 0.25 * 4;
-        //walk to 17,7
+    //walk to 17,7
     } else if (this.x < Math.ceil(this.x_offset + this.size * 17)
         && this.y === Math.ceil(this.y_offset + this.size * 7)) {
         this.direction = 1;
         this.x += 0.25 * 4;
     }
-	
 
-    ////walk to 0,0
-    //} else if (this.x === this.x_offset && this.y > this.y_offset) {
-    //    this.direction = 3;
-    //    this.y -= 0.25 * 4;
-    //}
-    
-
- //   if (this.x < Math.ceil(this.x_offset + this.size * 3) && this.y === this.y_offset + this.size * 5) {
-	//	this.direction = 1;
-	//	this.x += 1;
-	//}
-	
- //   if (this.rannum === 0 && this.x > Math.ceil(this.x_offset + this.size * 7)){
-	//	this.direction = 3;
-	//	this.y -= 1;
-	//}
-	//if (this.rannum === 1 && this.x >= Math.ceil(this.x_offset + this.size * 3)){
-	//	this.direction = 4;
-	//	this.y += 1;
-	//}
- //   if (this.rannum === 1 && this.y >= Math.ceil(this.y_offset + this.size * 7)){
-	//	this.direction = 1;
-	//	this.x += 1;
- //       this.y -= 1;
- //   }
-
+    //make it paralle to board axis instead of actuall axis
     var changedX = Math.ceil((this.x - this.offset) / this.size);
     if (changedX != this.boardX) {
         this.boardX = changedX;
@@ -527,18 +545,14 @@ ASSET_MANAGER.downloadAll(function () {
     console.log(gameboard);
     gameEngine.addEntity(gameboard);
     gameEngine.init(ctx);
-
     console.log("GAME entities count = " + gameEngine.entities.length);
 
-    //add attacker
+    //add attacker total 5 for now
     var attacker = new attackDude(gameEngine);
     gameEngine.addEntity(attacker);
-	
     var timesRun = 1;
-
     gameEngine.start();
-
-    //repeatly add attacker
+    //repeatly add attacker after game start
     var interval = setInterval(function () {
         var attacker = new attackDude(gameEngine);
         gameEngine.addEntity(attacker);
